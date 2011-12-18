5339b09a7db9f3c4ac535e22e2980b8f8caabb5a
TK Kocheran <rfkrocktk@gmail.com>
2011-12-18 10:19:30 -0800

    Fixed bug with HTTPS.

--- jQuerify.user.js
+++ jQuerify.user.js
@@ -8,34 +8,35 @@
 // @license         GPL http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
-    var scripts = document.getElementsByTagName("script");
-    var script = null;
+var scripts = document.getElementsByTagName("script");
+var script = null;
 
-    var found = false;
+var found = false;
 
-    for (var i = 0; i < scripts.length; i++) {
-        script = scripts[i];
-//      TODO this is still kind of loose, it'll match jQuery plugins.
-        if (/^jQuery.*\.js$/i.test(script.src)) {
-            found = true;
-            break;
-        }
+for (var i = 0; i < scripts.length; i++) {
+    script = scripts[i];
+//  TODO this is still kind of loose, it'll match jQuery plugins.
+    if (/^jQuery.*\.js$/i.test(script.src)) {
+        found = true;
+        break;
     }
+}
 
-    if (!found) {
-        try {
-            $ || jQuery || $ === jQuery;
-            found = true;
-        } catch (err) {
-            
-        }
+if (!found) {
+    try {
+        $ || jQuery || $ === jQuery;
+        found = true;
+    } catch (err) {
+        
     }
+}
 
-    if (!found) {
-        console.log("injecting jQuery");
-//      inject it.
-        script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
-        document.getElementsByTagName("body")[0].appendChild(script);
-    }
+if (!found) {
+//  inject it.
+    script = document.createElement("script");
+    script.type = "text/javascript";
+    
+    var protocol = /^https:/i.test(document.location) ? "https" : "http";
+    script.src = protocol + "://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+    document.getElementsByTagName("body")[0].appendChild(script);
+}
