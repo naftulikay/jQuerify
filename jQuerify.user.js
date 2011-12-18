afadbe364dc0cd00b3cde6fe29fdb3a1d4aac6ab
TK Kocheran <rfkrocktk@gmail.com>
2011-12-18 09:14:43 -0800

    Initial commit.

---
+++ jQuerify.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name            jQuerify
+// @namespace       http://tkassembled.com/ns/jQuerify
+// @description     Injects jQuery into all pages which don't already have it installed. 
+// @include         *
+// @copyright       TK Kocheran
+// @version         1.0.0
+// @license         GPL http://www.gnu.org/copyleft/gpl.html
+// ==/UserScript==
+
+document.addEventListener("ready", function() {
+    var scripts = document.getElementsByTagName("script");
+    var script = null;
+
+    var found = false;
+
+    for (var i = 0; i < scripts.length; i++) {
+        script = scripts[i];
+//      TODO this is still kind of loose, it'll match jQuery plugins.
+        if (/^jQuery.*\.js$/i.test(script.src)) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        try {
+            $ || jQuery || $ === jQuery;
+            found = true;
+        } catch (err) {
+            
+        }
+    }
+
+    if (!found) {
+        console.log("injecting jQuery");
+//      inject it.
+        script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+        document.getElementsByTagName("body")[0].appendChild(script);
+    }
+});
